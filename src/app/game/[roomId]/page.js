f08c074ee21cf3f9e61b325e1068a8dd434f9436
myatinsharma <39796588+myatinsharma@users.noreply.github.com>
2024-10-03 18:40:52 -0400

    Update page.js

--- src/app/game/[roomId]/page.js
+++ src/app/game/[roomId]/page.js
@@ -30,7 +30,7 @@ export default function GameRoom({ params }) {
     if (!roomId) return;
 
     // Initialize the socket connection once
-    socket = io("https://xyzatin.xyz");
+    socket = io("https://24.98.250.112");
 
     // Join the specific room
     socket.emit("joinRoom", roomId);
